14059cdff0d4fb9247b3f7d8e52119b4ecc3bc17
Maciej Krysztofiak <maciej.krysztofiak@codeclutch.dev>
2022-02-11 08:15:00 +0100

    add finding order content

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\OrderContent;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -16,4 +17,8 @@ class Order extends Model
         'sent',
         'delivered'
     ];
+
+    public function content(){
+        return $this->hasMany(OrderContent::class);
+     }
 }
